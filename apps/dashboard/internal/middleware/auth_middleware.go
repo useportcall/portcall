21265dca252b579f2173b7c09befc63df4e59626
ceriksson <carleriksson92@gmail.com>
2025-11-08 18:41:29 +0000

    add dashboard auth middleware

---
+++ apps/dashboard/internal/middleware/auth_middleware.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/useportcall/portcall/libs/go/authx"
+	"github.com/useportcall/portcall/libs/go/dbx"
+	"github.com/useportcall/portcall/libs/go/dbx/models"
+)
+
+func Auth(db dbx.IORM) gin.HandlerFunc {
+	client := authx.New()
+
+	return func(c *gin.Context) {
+		email, err := client.Validate(c.Request.Context(), c.Request.Header)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"access": "unauthorized"})
+			c.Abort()
+			return
+		}
+
+		c.Set("auth_email", email)
+
+		if c.Param("app_id") == "" {
+			c.Next()
+			return
+		}
+
+		var app models.App
+		if err := db.FindFirst(&app, "public_id = ?", c.Param("app_id")); err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "App not found"})
+			c.Abort()
+			return
+		}
+
+		c.Set("app_id", app.ID)
+
+		c.Next()
+	}
+}
--- apps/dashboard/internal/router/router.go
+++ apps/dashboard/internal/router/router.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/useportcall/portcall/apps/dashboard/internal/middleware"
 	"github.com/useportcall/portcall/libs/go/cryptox"
 	"github.com/useportcall/portcall/libs/go/dbx"
 	"github.com/useportcall/portcall/libs/go/qx"
@@ -15,6 +16,10 @@ func Init(db dbx.IORM, crypto cryptox.ICrypto, q qx.IQueue) *gin.Engine {
 	r.SetTrustedProxies([]string{"127.0.0.1", "172.17.0.0/16"}) // TODO: review for production
 
 	r.GET("/ping", func(c *gin.Context) { c.JSON(http.StatusOK, gin.H{"message": "pong"}) })
+
+	api := r.Group("/api")
+
+	api.Use(middleware.Auth(db))
 
 	return r
 }
