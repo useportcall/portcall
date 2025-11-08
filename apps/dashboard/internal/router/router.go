a0b6ae67796e4c1ecae7066987d9ae70d2ea775d
ceriksson <carleriksson92@gmail.com>
2025-11-08 18:19:39 +0000

    add dashboard app

---
+++ apps/dashboard/internal/router/router.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/useportcall/portcall/libs/go/cryptox"
+	"github.com/useportcall/portcall/libs/go/dbx"
+	"github.com/useportcall/portcall/libs/go/qx"
+)
+
+func Init(db dbx.IORM, crypto cryptox.ICrypto, q qx.IQueue) *gin.Engine {
+	r := gin.Default() // TODO: replace defaults
+
+	r.SetTrustedProxies([]string{"127.0.0.1", "172.17.0.0/16"}) // TODO: review for production
+
+	r.GET("/ping", func(c *gin.Context) { c.JSON(http.StatusOK, gin.H{"message": "pong"}) })
+
+	return r
+}
---
+++ apps/dashboard/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/useportcall/portcall/apps/dashboard/internal/router"
+	"github.com/useportcall/portcall/libs/go/cryptox"
+	"github.com/useportcall/portcall/libs/go/dbx"
+	"github.com/useportcall/portcall/libs/go/envx"
+	"github.com/useportcall/portcall/libs/go/qx"
+)
+
+func main() {
+	envx.Load()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable not set")
+	}
+
+	db := dbx.New()
+	crypto := cryptox.New()
+	q := qx.New()
+
+	router := router.Init(db, crypto, q)
+
+	if err := router.Run(":" + port); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
