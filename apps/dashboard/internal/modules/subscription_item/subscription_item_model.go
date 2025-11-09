0f7c6ae40cca8268db148afc59973e6b655e7c3c
ceriksson <carleriksson92@gmail.com>
2025-11-09 18:32:16 +0000

    add subscription item module

---
+++ apps/dashboard/internal/modules/subscription_item/subscription_item_model.go
@@ -0,0 +1,22 @@
+package subscription_item
+
+import (
+	"time"
+
+	"github.com/useportcall/portcall/libs/go/dbx/models"
+)
+
+type SubscriptionItem struct {
+	ID        string    `db:"id" json:"-"`
+	Quantity  int32     `db:"quantity" json:"quantity"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+}
+
+func (si *SubscriptionItem) Set(subscriptionItem *models.SubscriptionItem) *SubscriptionItem {
+	si.ID = subscriptionItem.PublicID
+	si.Quantity = subscriptionItem.Quantity
+	si.CreatedAt = subscriptionItem.CreatedAt
+	si.UpdatedAt = subscriptionItem.UpdatedAt
+	return si
+}
