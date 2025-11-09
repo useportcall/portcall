571735c4fa7483faabc96df8aa0a471271124e8e
ceriksson <carleriksson92@gmail.com>
2025-11-09 15:32:26 +0000

    add dashboard pub id gen fn util

---
+++ apps/dashboard/internal/utils/utils.go
@@ -0,0 +1,11 @@
+package utils
+
+import (
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+func GenPublicID(prefix string) string {
+	return prefix + "_" + strings.ReplaceAll(uuid.New().String(), "-", "")
+}
