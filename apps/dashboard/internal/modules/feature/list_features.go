ae4ab4dfc818465d6f0fd64bc250a8a593978159
ceriksson <carleriksson92@gmail.com>
2025-11-09 18:13:25 +0000

    fix broken import

--- apps/dashboard/internal/modules/feature/list_features.go
+++ apps/dashboard/internal/modules/feature/list_features.go
@@ -1,8 +1,8 @@
 package feature
 
 import (
-	"github.com/useportcall/portcall/apps/dashboard/internal/routerx"
 	"github.com/useportcall/portcall/libs/go/dbx/models"
+	"github.com/useportcall/portcall/libs/go/routerx"
 )
 
 func ListFeatures(c *routerx.Context) {
