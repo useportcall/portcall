a3b3524630e3daf77cfa1a6dd4c6f2b4438ec062
ceriksson <carleriksson92@gmail.com>
2025-11-10 10:30:00 +0000

    add dashboard get user subscription query

---
+++ apps/dashboard/internal/modules/subscription/get_user_subscription.go
@@ -0,0 +1,30 @@
+package subscription
+
+import (
+	"github.com/useportcall/portcall/libs/go/dbx"
+	"github.com/useportcall/portcall/libs/go/dbx/models"
+	"github.com/useportcall/portcall/libs/go/routerx"
+)
+
+func GetUserSubscription(c *routerx.Context) {
+	userID := c.Param("id")
+
+	var user models.User
+	if err := c.DB().GetForPublicID(c.AppID(), userID, &user); err != nil {
+		c.ServerError("Failed to get user")
+		return
+	}
+
+	var subscription models.Subscription
+	if err := c.DB().FindFirst(&subscription, "app_id = ? AND user_id = ? AND status = 'active'", c.AppID(), user.ID); err != nil {
+		if dbx.IsRecordNotFoundError(err) {
+			c.OK(nil)
+			return
+		}
+
+		c.ServerError("Failed to get user subscription")
+		return
+	}
+
+	c.OK(new(Subscription).Set(&subscription))
+}
