2cae59a5335bdf88e5c16ae2244366474b170daa
ceriksson <carleriksson92@gmail.com>
2025-11-12 23:24:52 +0000

    add get entitlement handler api

---
+++ apps/api/internal/modules/v1/entitlement/get_entitlement.go
@@ -0,0 +1,31 @@
+package entitlement
+
+import (
+	"github.com/useportcall/portcall/libs/go/apix"
+	"github.com/useportcall/portcall/libs/go/dbx"
+	"github.com/useportcall/portcall/libs/go/dbx/models"
+	"github.com/useportcall/portcall/libs/go/routerx"
+)
+
+func GetEntitlement(c *routerx.Context) {
+	userID := c.Param("user_id")
+	entitlementID := c.Param("id")
+
+	query := "app_id = ? AND user_id = ? AND feature_public_id = ?"
+
+	var user models.User
+	if err := c.DB().GetForPublicID(c.AppID(), userID, &user); err != nil {
+		c.NotFound("User not found")
+		return
+	}
+
+	var entitlement models.Entitlement
+	if err := c.DB().FindFirst(&entitlement, query, c.AppID(), user.ID, entitlementID); err != nil {
+		if !dbx.IsRecordNotFoundError(err) {
+			c.ServerError("Internal server error")
+			return
+		}
+	}
+
+	c.OK(new(apix.Entitlement).Set(&entitlement))
+}
