3c388c8382036b7d2647285365c4a7c3eef85fd2
ceriksson <carleriksson92@gmail.com>
2025-11-12 23:21:00 +0000

    add models to apix

---
+++ libs/go/apix/entitlement.go
@@ -0,0 +1,28 @@
+package apix
+
+import (
+	"time"
+
+	"github.com/useportcall/portcall/libs/go/dbx/models"
+)
+
+type Entitlement struct {
+	ID          string     `json:"id"`
+	Usage       int64      `json:"usage"`
+	Quota       int64      `json:"quota"`
+	Interval    string     `json:"interval"`
+	NextResetAt *time.Time `json:"next_reset_at"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+}
+
+func (e *Entitlement) Set(entitlement *models.Entitlement) *Entitlement {
+	e.ID = entitlement.FeaturePublicID
+	e.Interval = entitlement.Interval
+	e.Quota = entitlement.Quota
+	e.Usage = entitlement.Usage
+	e.NextResetAt = entitlement.NextResetAt
+	e.CreatedAt = entitlement.CreatedAt
+	e.UpdatedAt = entitlement.UpdatedAt
+	return e
+}
