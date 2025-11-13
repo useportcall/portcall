3e2d2d84ef4ad466d0cbc704dc762529606c46d6
ceriksson <carleriksson92@gmail.com>
2025-11-13 23:06:45 +0000

    add enabled bool to entitlement api

--- libs/go/apix/entitlement.go
+++ libs/go/apix/entitlement.go
@@ -11,6 +11,7 @@ type Entitlement struct {
 	Usage       int64      `json:"usage"`
 	Quota       int64      `json:"quota"`
 	Interval    string     `json:"interval"`
+	Enabled     bool       `json:"enabled"`
 	NextResetAt *time.Time `json:"next_reset_at"`
 	CreatedAt   time.Time  `json:"created_at"`
 	UpdatedAt   time.Time  `json:"updated_at"`
@@ -24,5 +25,12 @@ func (e *Entitlement) Set(entitlement *models.Entitlement) *Entitlement {
 	e.NextResetAt = entitlement.NextResetAt
 	e.CreatedAt = entitlement.CreatedAt
 	e.UpdatedAt = entitlement.UpdatedAt
+
+	if entitlement.Quota == -1 {
+		e.Enabled = true
+	} else {
+		e.Enabled = entitlement.Usage < entitlement.Quota
+	}
+
 	return e
 }
