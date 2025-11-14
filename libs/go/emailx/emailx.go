1ee9de3d12813a3d24453872a230bab73a0ab95e
ceriksson <carleriksson92@gmail.com>
2025-11-14 00:41:50 +0000

    add email module

---
+++ libs/go/emailx/emailx.go
@@ -0,0 +1,47 @@
+package emailx
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/smtp"
+	"os"
+	"text/template"
+)
+
+func SendTemplateEmail(payload []byte, tmplFileName, subject, from string, to []string) error {
+	var body any
+	if err := json.Unmarshal(payload, &body); err != nil {
+		return err
+	}
+
+	tmpl, err := template.ParseFiles(tmplFileName)
+	if err != nil {
+		log.Fatal("failed to parse template:", err)
+	}
+
+	var htmlContentBuf bytes.Buffer
+	if err := tmpl.Execute(&htmlContentBuf, body); err != nil {
+		log.Fatal("failed to execute template:", err)
+	}
+	htmlContent := htmlContentBuf.String()
+
+	msg := []byte("" +
+		"From: " + from + "\r\n" +
+		"To: " + to[0] + "\r\n" +
+		"Subject: " + subject + "\r\n" +
+		"MIME-Version: 1.0\r\n" +
+		"Content-Type: text/html; charset=UTF-8\r\n" +
+		"\r\n" +
+		htmlContent +
+		"\r\n")
+
+	// No auth, no TLS; MailHog/MailDev accept this by default
+	if err := smtp.SendMail(os.Getenv("SMTP_SERVER"), nil, from, to, msg); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Email sent")
+
+	return nil
+}
---
+++ libs/go/emailx/go.mod
@@ -0,0 +1,3 @@
+module github.com/useportcall/portcall/libs/go/emailx
+
+go 1.25.4
