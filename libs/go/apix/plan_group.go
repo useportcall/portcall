9b4323c47b36216fdb14d999e8fb43aef0a22efc
ceriksson <carleriksson92@gmail.com>
2025-11-15 23:57:36 +0000

    add plan group model apix

---
+++ libs/go/apix/plan_group.go
@@ -0,0 +1,14 @@
+package apix
+
+import "github.com/useportcall/portcall/libs/go/dbx/models"
+
+type PlanGroup struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func (pg *PlanGroup) Set(planGroup *models.PlanGroup) *PlanGroup {
+	pg.ID = planGroup.PublicID
+	pg.Name = planGroup.Name
+	return pg
+}
