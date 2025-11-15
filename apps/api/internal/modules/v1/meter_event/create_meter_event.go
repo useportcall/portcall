bb494daf603e214189314e2f940b62c7b4a87ee3
ceriksson <carleriksson92@gmail.com>
2025-11-15 23:50:49 +0000

    fix logic create meter event api

--- apps/api/internal/modules/v1/meter_event/create_meter_event.go
+++ apps/api/internal/modules/v1/meter_event/create_meter_event.go
@@ -29,6 +29,19 @@ func CreateMeterEvent(c *routerx.Context) {
 		return
 	}
 
+	var entitlement models.Entitlement
+	if err := c.DB().FindFirst(&entitlement, "app_id = ? AND feature_public_id = ?", c.AppID(), body.FeatureID); err != nil {
+		c.ServerError("Entitlement not found", err)
+		return
+	}
+
+	log.Println("Current entitlement usage:", entitlement.Usage, "Quota:", entitlement.Quota, "Requested usage increment:", body.Usage)
+
+	if entitlement.Quota > 0 && (entitlement.Usage+body.Usage) > entitlement.Quota && body.Usage > 0 {
+		c.BadRequest("Usage exceeds entitlement quota")
+		return
+	}
+
 	var feature models.Feature
 	if err := c.DB().GetForPublicID(c.AppID(), body.FeatureID, &feature); err != nil {
 		log.Println("Feature not found:", err)
