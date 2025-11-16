29ba06e8c347651bfe201e64081ba57bd9baecaf
ceriksson <carleriksson92@gmail.com>
2025-11-16 22:07:03 +0000

    add cron logic to cron app

--- apps/cron/go.mod
+++ apps/cron/go.mod
@@ -1,3 +1,10 @@
 module github.com/useportcall/portcall/apps/cron
 
 go 1.25.4
+
+require (
+	github.com/go-co-op/gocron/v2 v2.18.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jonboulle/clockwork v0.5.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+)
--- apps/cron/main.go
+++ apps/cron/main.go
@@ -1,3 +1,49 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/go-co-op/gocron/v2"
+	"github.com/useportcall/portcall/libs/go/envx"
+	"github.com/useportcall/portcall/libs/go/qx"
+)
+
+func main() {
+	envx.Load()
+
+	s, err := gocron.NewScheduler(
+		gocron.WithLocation(time.UTC),
+		// optional: WithLogger, WithMonitor, WithDistributedJobLocker…
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	q := qx.New()
+
+	_, err = s.NewJob(
+		gocron.CronJob("*/10 * * * * *", true),
+		gocron.NewTask(func() {
+			log.Println("Finding subscriptions to reset...")
+			q.Enqueue("find_subscriptions_to_reset", map[string]interface{}{}, "billing_queue")
+		}),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s.Start()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("shutting down…")
+	if err := s.Shutdown(); err != nil {
+		log.Printf("scheduler shutdown error: %v", err)
+	}
+}
